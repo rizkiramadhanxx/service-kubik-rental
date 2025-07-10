e6bb770b50d5e8b8e560d8b821f6b62f4c72cd4d
Rizki Ramadhan <rizkiramdhanx@gmail.com>
2025-07-10 11:44:56 +0700

    revisi

--- feature/device/routes.go
+++ feature/device/routes.go
@@ -6,10 +6,11 @@ import (
 
 func SetupRoutes(router fiber.Router) {
 	router.Get("/", GetAllDevices)
+	router.Post("/multiple-ping", PingMultipleDevices)
 	router.Get("/ping/:id", PingDevice)
 	router.Get("/action/:id", ActionToDevice)
-	router.Get("/:id", GetDevice)
 	router.Post("/", CreateDevice)
-	router.Put("/:id", UpdateDevice)
+	router.Patch("/:id", UpdateDevice)
 	router.Delete("/:id", DeleteDevice)
+	router.Get("/:id", GetDevice) // <- pindahkan ini ke paling bawah
 }
